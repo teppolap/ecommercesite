0dd579e7bccf8e0ea135c1a2d36e9306c71e8011
teppolap <teppo.lappalainen28@gmail.com>
2025-01-22 12:30:09 +0200

    ..

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  eslint: {
+    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
 };
 
 export default nextConfig;
